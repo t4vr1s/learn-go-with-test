516ba4f4672a17a5993b271d5d69c9944e5dea7a
ees <eescobar@computin.cl>
2021-05-03 00:55:13 -0400

    add test for map

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,10 +1,17 @@
 package main
 
-import "errors"
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were lookin for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update because it does not exist")
+)
 
 type Dictionary map[string]string
+type DictionaryErr string
 
-var ErrNotFound = errors.New("could not find the word you were lookin for")
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -14,8 +21,34 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
 
 // func Search(dictionary map[string]string, word string) string {
@@ -26,4 +59,3 @@ func (d Dictionary) Add(word, definition string) {
 // 	}
 // }
 // return dictionary[word]
-// }
